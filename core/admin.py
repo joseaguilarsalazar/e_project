e526f5339233570286ec995a2b2206e767d915ef
Jose <joseaguilarsalazar2004@gmail.com>
2025-07-31 17:31:05 -0500

    first commit

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
+# admin.py
 from django.contrib import admin
+from .models import (
+    Notification, Company, Rol, UserCompany, Ship, SeatType, 
+    Seat, Route, Trip, TripSeat, Booking, PaymentMethod, Payment
+)
 
-# Register your models here.
+admin.site.register(Notification)
+admin.site.register(Company)
+admin.site.register(Rol)
+admin.site.register(UserCompany)
+admin.site.register(Ship)
+admin.site.register(SeatType)
+admin.site.register(Seat)
+admin.site.register(Route)
+admin.site.register(Trip)
+admin.site.register(TripSeat)
+admin.site.register(Booking)
+admin.site.register(PaymentMethod)
+admin.site.register(Payment)
